49f8878d91787a0684ae476e2be3b35b2ab19860
Tointer <Tointer@yandex.ru>
2025-06-24 10:46:27 +0300

    make app close into tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,17 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-use std::sync::mpsc::{channel, Sender};
+use std::sync::mpsc::{Sender};
 use std::sync::Mutex;
 use std::thread::JoinHandle;
 
 use once_cell::sync::Lazy;
 mod commands;
 mod utils;
+
+use tauri::{
+  menu::{Menu, MenuItem},
+  tray::{TrayIconBuilder},
+  Manager, WindowEvent
+};
 
 struct WatcherHandle {
     stop: Sender<()>,
@@ -32,6 +38,35 @@ pub fn run() {
             commands::watching::start_watching,
             commands::watching::stop_watching,
         ])
+        .setup(|app| {
+            /* -------- tray menu -------- */
+            let show = MenuItem::with_id(app, "show", "Show window", true, None::<&str>)?;
+            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&show, &quit])?;
+
+            /* -------- tray icon -------- */
+            TrayIconBuilder::new()
+                .menu(&menu)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                "show" => {
+                    if let Some(win) = app.get_webview_window("main") {
+                    let _ = win.show();
+                    let _ = win.set_focus();
+                    }
+                }
+                "quit" => app.exit(0),
+                _ => {}
+                })
+                .build(app)?;
+
+            Ok(())
+        })
+        .on_window_event(|window, event| {
+            if let WindowEvent::CloseRequested { api, .. } = event {
+                window.hide().unwrap();
+                api.prevent_close();
+            }
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
